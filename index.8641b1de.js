ed96eb5e2c9ee6c73ab633c54385f1a22715b553
annaabramovaa <abramovaana193@gmail.com>
2025-03-06 12:21:22 +0200

    make build

---
+++ index.8641b1de.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").addEventListener("click",function(e){e.preventDefault();var t=document.querySelector(".gallery__large-img"),a=e.target;"IMG"===a.tagName&&(a=a.parentElement),"A"===a.tagName&&(t.src=a.href)});
+//# sourceMappingURL=index.8641b1de.js.map
