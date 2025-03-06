1d95f235a548c2b24dc21ab14c81f5f7236e0612
annaabramovaa <abramovaana193@gmail.com>
2025-03-06 12:16:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.getElementById('thumbs').addEventListener('click', function () {
+  event.preventDefault();
+
+  const bigImg = document.querySelector('.gallery__large-img');
+  let target = event.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  if (target.tagName === 'A') {
+    bigImg.src = target.href;
+  }
+});
